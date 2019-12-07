a479244787855d63cf5f5f59fc8ea155a9ca3f8a
Ray <coolrey3@gmail.com>
2019-12-07 17:24:33 -0500

    manual case correction

--- pokerbot.py
+++ pokerbot.py
@@ -2,7 +2,7 @@ import discord
 from poker import *
 from discord.ext import commands
 
-bot = commands.Bot(command_prefix='!', case_insensitive=True)
+#bot = commands.Bot(command_prefix='!', case_insensitive=True)
 
 
 #Discord
@@ -39,7 +39,7 @@ async def on_message(message):
     if message.content.find("!users") !=-1:
         await message.channel.send(f"""# of users on this server: {guildId.member_count}""")
 
-    if message.content.find("!pb" or "!pokerbot") !=-1 #or message.content.find("!pokerbot") !=-1:
+    if message.content.find("!pb") !=-1 or message.content.find("!pokerbot") !=-1:
         await message.channel.send("Hi i'm pokerbot! I am in early beta so what I am able to do is limited at this time. Refer"
                                    " to the list below for my available commands. \n\n"
                                    "'!open range' : lists NLHE opening ranges \n"
@@ -49,81 +49,86 @@ async def on_message(message):
         await message.channel.send("https://cdn.discordapp.com/attachments/635691657219538945/652081217679917088/opening_ranges.jpg",)
 
 
-    #if message.content.find("!equity") !=-1:
+#if message.content.find("!equity") !=-1:
     #
     #     holecards = message.content[8:]
     #     print(holecards)
     #     print(cards[0])
 
-    #best method using ranges and poker library
-        if Hand("33") in Range('AA-22'):
-            print('its here')
-        else:
-            print("not here")
+#best method using ranges and poker library
+    # if Hand("33") in Range('AA-22'):
+    #     print('its here')
+    # else:
+    #     print("not here")
 
 
-    #1 method of finding cards via count
-        if cards.count(holecards) > 0:
-            print("found em")
-        else:
-            print(type(cards))
-            print(type(holecards))
-            print(type(cards[0]))
+#1 method of finding cards via count
+    # if cards.count(holecards) > 0:
+    #     print("found em")
+    # else:
+    #     print(type(cards))
+    #     print(type(holecards))
+    #     print(type(cards[0]))
 
-    #checks cards in array, will likely use holes variable and search respective array per position
-        if holecards in cards:
-            await message.channel.send(holecards + ' , All in Boss')
-        else:
-            await message.channel.send(holecards + " ,Fold it")
+#checks cards in array, will likely use holes variable and search respective array per position
+    # if holecards in cards:
+    #     await message.channel.send(holecards + ' , All in Boss')
+    # else:
+    #     await message.channel.send(holecards + " , Fold it")
 
 #UTG Open Check
     if message.content.find("!UTG") !=-1:
         holecards = message.content[5:]
         print(holecards)
         if holecards in utgOpen:
-            await message.channel.send(holecards + ' UTG, GTO says open')
+            await message.channel.send(holecards + ' Under the Gun, GTO says open')
         else:
-            await message.channel.send(holecards + " UTG,Fold it")
+            await message.channel.send(holecards + " Under the Gun, GTO says Fold it")
 
 #MP Open Check
     if message.content.find("!MP") !=-1:
         holecards = message.content[4:]
         print(holecards)
         if holecards in mpOpen:
-            await message.channel.send(holecards + ' MP, GTO says open')
+            await message.channel.send(holecards + ' in MP, GTO says open')
         else:
-            await message.channel.send(holecards + " MP,Fold it")
+            await message.channel.send(holecards + " in MP, GTO says Fold it")
 
 #CO Open Check
     if message.content.find("!CO") !=-1:
         holecards = message.content[4:]
         print(holecards)
         if holecards in coOpen:
-            await message.channel.send(holecards + ' CO, GTO says open')
+            await message.channel.send(holecards + ' in the CO, GTO says open')
         else:
-            await message.channel.send(holecards + " CO,Fold it")
+            await message.channel.send(holecards + " in the CO, GTO says Fold it")
 
 #SB Open Check
     if message.content.find("!SB") !=-1:
         holecards = message.content[4:]
         print(holecards)
         if holecards in sbOpen:
-            await message.channel.send(holecards + ' SB, GTO says open')
+            await message.channel.send(holecards + ' in the SB, GTO says open')
         else:
-            await message.channel.send(holecards + " SB,Fold it")
+            await message.channel.send(holecards + " in the SB, GTO says Fold it")
 
 #Button Open Check
     if message.content.find("!Button") !=-1:
         holecards = message.content[8:]
         print(holecards)
         if holecards in buttonOpen:
-            await message.channel.send(holecards + ' Button, GTO says open')
+            await message.channel.send(holecards + ' on the Button, GTO says open')
+
         else:
-            await message.channel.send(holecards + " Button,Fold it")
+            await message.channel.send(holecards + " on the Button, GTO says Fold it")
+    elif message.content.find("!BTN") != -1 or message.content.find("!btn") != -1:
+        holecards = message.content[5:]
+        print(holecards)
+        if holecards in buttonOpen:
+            await message.channel.send(holecards + ' on the Button, GTO says open')
 
-
-
-
+        else:
+            await message.channel.send(holecards + " on the Button, GTO says Fold it")
 
 
 client.run(token)
