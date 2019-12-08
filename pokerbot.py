b7fb30f81977531c92a71da8cc386f24b60b970a
Ray <coolrey3@gmail.com>
2019-12-08 04:00:04 -0500

    modified the execution from on message to bot commands, improved performance

--- pokerbot.py
+++ pokerbot.py
@@ -1,9 +1,11 @@
 import discord
 from poker import *
-#from discord.ext import commands
+from discord.ext import commands
 
-#bot = commands.Bot(command_prefix='!', case_insensitive=True)
+description = '''An example bot to showcase the discord.ext.commands extension
+module. There are a number of utility commands being showcased here.'''
 
+bot = commands.Bot(command_prefix='!', case_insensitive=True, description=description, pass_context=True)
 
 #Discord
 def read_token():
@@ -12,8 +14,8 @@ def read_token():
         return lines[0].strip()
 
 token = read_token()
-client=discord.Client()
-
+client = discord.Client()
+guildId = client.get_guild(635691275663704091)
 
 #GTO Opening Ranges
 utgOpen = Range('AA-33 AKo-AJo KQo AKs-ATs KQs-KTs QJs-QTs JTs-J9s T9s 98s 87s 76s 65s')
@@ -22,116 +24,67 @@ coOpen = Range('AA-22 AKs-A2s KQs-K6s QJs-Q7s JTs-J8s T9s-T8s 98s-97s 87s-86s 76
 buttonOpen = Range('AA-22 AKs-A2s KQs-K2s QJs-Q2s JTs-J5s T9s-T6s 98s-96s 87s-85s 76s-74s 65s-64s 54s-53s 43s AKo-A2o KQo-K7o QJo-Q9o JTo-J9o T9o-T8o 98o 87o')
 sbOpen = Range('AA-22 AKs-A2s KQs-K2s QJs-Q4s JTs-J7s T9s-T7s 98s-97s 87s-86s 76s-75s 65s-64s 54s AKo-A7o KQo-K9o QJo-Q9o JTo-J9o T9o 98o')
 
-@client.event
-async def on_message(message):
+@bot.event
+async def on_ready():
+    print('Logged in as')
+    print(bot.user.name)
+    print(bot.user.id)
+    print('------')
 
-    guildId=client.get_guild(635691275663704091)
-
-    # we do not want the bot to reply to itself
-    if message.author == client.user:
-        return
-
-    #Uncomment so bot does not respond to other bots
-    #if message.author.bot:
-        #return
-
-    # Bot Commands
-
-    if message.content.find("!users") !=-1:
-        await message.channel.send(f"""# of users on this server: {guildId.member_count}""")
-
-    if message.content.find("!pb") !=-1 or message.content.find("!pokerbot") !=-1:
-        await message.channel.send("Hi i'm pokerbot! I am in early beta so what I am able to do is limited at this time. Refer"
+@bot.command()
+async def pb(ctx):
+    await ctx.send("Hi i'm pokerbot! I am in early beta so what I am able to do is limited at this time. Refer"
                                    " to the list below for my available commands. \n\n"
                                    "'!open range' : lists NLHE opening ranges \n"
                                    "'!equity' : type command followed by hole cards to return equity;(i.e: !equity A9o) \n")
 
-    if message.content.find("!open range") !=-1:
-        await message.channel.send("https://cdn.discordapp.com/attachments/635691657219538945/652081217679917088/opening_ranges.jpg",)
+@bot.command()
+async def openrange(ctx):
+    await ctx.send("https://cdn.discordapp.com/attachments/635691657219538945/652081217679917088/opening_ranges.jpg")
 
+@bot.command()
+async def users(ctx):
+    await ctx.send(f"""# of users on this server: {guildId.member_count}""")
 
-#if message.content.find("!equity") !=-1:
-    #
-    #     holecards = message.content[8:]
-    #     print(holecards)
-    #     print(cards[0])
-
-#best method using ranges and poker library
-    # if Hand("33") in Range('AA-22'):
-    #     print('its here')
-    # else:
-    #     print("not here")
-
-
-#1 method of finding cards via count
-    # if cards.count(holecards) > 0:
-    #     print("found em")
-    # else:
-    #     print(type(cards))
-    #     print(type(holecards))
-    #     print(type(cards[0]))
-
-#checks cards in array, will likely use holes variable and search respective array per position
-    # if holecards in cards:
-    #     await message.channel.send(holecards + ' , All in Boss')
-    # else:
-    #     await message.channel.send(holecards + " , Fold it")
-
-#UTG Open Check
-    if message.content.find("!UTG") !=-1:
-        holecards = message.content[5:]
-        print(holecards)
-        if holecards in utgOpen:
-            await message.channel.send(holecards + ' Under the Gun, GTO says open')
+@bot.command()
+async def sb(ctx,arg):
+        print(arg)
+        if arg in sbOpen:
+            await ctx.send(arg + ' in the SB, GTO says open')
         else:
-            await message.channel.send(holecards + " Under the Gun, GTO says Fold it")
+            await ctx.send(arg + " in the SB, GTO says Fold it")
 
-#MP Open Check
-    if message.content.find("!MP") !=-1:
-        holecards = message.content[4:]
-        print(holecards)
-        if holecards in mpOpen:
-            await message.channel.send(holecards + ' in MP, GTO says open')
-        else:
-            await message.channel.send(holecards + " in MP, GTO says Fold it")
+@bot.command()
+async def utg(ctx,arg):
+    print(arg)
+    if arg in utgOpen:
+        await ctx.send(arg + ' Under the Gun, GTO says open')
+    else:
+        await ctx.send(arg + " Under the Gun, GTO says Fold it")
 
-#CO Open Check
-    if message.content.find("!CO") !=-1:
-        holecards = message.content[4:]
-        print(holecards)
-        if holecards in coOpen:
-            await message.channel.send(holecards + ' in the CO, GTO says open')
-        else:
-            await message.channel.send(holecards + " in the CO, GTO says Fold it")
+@bot.command()
+async def mp(ctx, arg):
+    print(arg)
+    if arg in mpOpen:
+        await ctx.send(arg + ' in MP, GTO says open')
+    else:
+        await ctx.send(arg + " in MP, GTO says Fold it")
 
-#SB Open Check
-    if message.content.find("!SB") !=-1:
-        holecards = message.content[4:]
-        print(holecards)
-        if holecards in sbOpen:
-            await message.channel.send(holecards + ' in the SB, GTO says open')
-        else:
-            await message.channel.send(holecards + " in the SB, GTO says Fold it")
+@bot.command()
+async def co(ctx, arg):
+    print(arg)
+    if arg in coOpen:
+        await ctx.send(arg + ' in the CO, GTO says open')
+    else:
+        await ctx.send(arg + " in the CO, GTO says Fold it")
 
-#Button Open Check
-    if message.content.find("!Button") !=-1:
-        holecards = message.content[8:]
-        print(holecards)
-        if holecards in buttonOpen:
-            await message.channel.send(holecards + ' on the Button, GTO says open')
+@bot.command()
+async def btn(ctx, arg):
+    print(arg)
+    if arg in buttonOpen:
+        await ctx.send(arg + ' on the Button, GTO says open')
 
-        else:
-            await message.channel.send(holecards + " on the Button, GTO says Fold it")
-    elif message.content.find("!BTN") != -1 or message.content.find("!btn") != -1:
-        holecards = message.content[5:]
-        print(holecards)
-        if holecards in buttonOpen:
-            await message.channel.send(holecards + ' on the Button, GTO says open')
+    else:
+        await ctx.send(arg + " on the Button, GTO says Fold it")
 
-        else:
-            await message.channel.send(holecards + " on the Button, GTO says Fold it")
-
-
-client.run(token)
-
-
+bot.run(token)
