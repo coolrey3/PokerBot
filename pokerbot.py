a4bd8c00ecc39e388cad9e7e175908f75fe5b0c8
Ray <coolrey3@gmail.com>
2019-12-10 18:24:10 -0500

    created class , still calling from within file, minor changes

--- pokerbot.py
+++ pokerbot.py
@@ -6,7 +6,6 @@ import os.path
 from PIL import Image
 
 
-
 description = '''An example bot to showcase the discord.ext.commands extension
 module. There are a number of utility commands being showcased here.'''
 
@@ -29,8 +28,7 @@ guildId = client.get_guild(635691275663704091)
 utgOpen = Range('AA-33 AKo-AJo KQo AKs-ATs KQs-KTs QJs-QTs JTs-J9s T9s 98s 87s 76s 65s')
 mpOpen = Range('AA-22 AKs-A7s A5s KQs-KTs QJs-QTs JTs-J9s T9s-T8s 98s-97s 87s-86s 76s-75s 65s 54s AKo-ATo KQo')
 coOpen = Range('AA-22 AKs-A2s KQs-K6s QJs-Q7s JTs-J8s T9s-T8s 98s-97s 87s-86s 76s-75s 65s-64s 54s AKo-ATo KQo-KJo QJo')
-buttonOpen = Range(
-    'AA-22 AKs-A2s KQs-K2s QJs-Q2s JTs-J5s T9s-T6s 98s-96s 87s-85s 76s-74s 65s-64s 54s-53s 43s AKo-A2o KQo-K7o QJo-Q9o JTo-J9o T9o-T8o 98o 87o')
+buttonOpen = Range('AA-22 AKs-A2s KQs-K2s QJs-Q2s JTs-J5s T9s-T6s 98s-96s 87s-85s 76s-74s 65s-64s 54s-53s 43s AKo-A2o KQo-K7o QJo-Q9o JTo-J9o T9o-T8o 98o 87o')
 sbOpen = Range(
     'AA-22 AKs-A2s KQs-K2s QJs-Q4s JTs-J7s T9s-T7s 98s-97s 87s-86s 76s-75s 65s-64s 54s AKo-A7o KQo-K9o QJo-Q9o JTo-J9o T9o 98o')
 
