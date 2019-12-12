00ab0d408ee5eaa7d1512af6317d3d94bcb2b5c2
Ray <coolrey3@gmail.com>
2019-12-12 17:58:11 -0500

    fixed GTO value to be function

--- Results/__init__.py
+++ Results/__init__.py
@@ -6,7 +6,7 @@ class Results:
         gt = getattr(PositionRange, f'''{position}Open''')
         gtoOutput = gt
 
-        print(gtoOutput)
+        # print(gtoOutput)
         if cards in gtoOutput:
             GTOplay = 'Open'
             return GTOplay
@@ -14,7 +14,9 @@ class Results:
             GTOplay = 'Fold'
             return GTOplay
 
+
+
     async def run(ctx,cards,position,stack):
 
-            await ctx.send(f'''Position: **{str.upper(position)}**   \nStack: **{stack}**  \nGTO: **{Results.GTO(cards,position)}**\n\n **__Raise First-In__**\n\n **__Call VS__**\n\n **__3-Bet VS__**\n\n **__Squeeze VS__**''')
+            await ctx.send(f'''Position: **{str.upper(position)}**   \nStack: **{stack}**  \nGTO: **__{Results.GTO(cards,position)}__**\n\n **__Raise First-In__**\n\n **__Call VS__**\n\n **__3-Bet VS__**\n\n **__Squeeze VS__**''')
 
