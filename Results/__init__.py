7f3421fbe6b2d3b438e56394cf99f30ba0eb9172
Ray <coolrey3@gmail.com>
2019-12-12 05:36:23 -0500

    added squeeze etc

--- Results/__init__.py
+++ Results/__init__.py
@@ -5,23 +5,15 @@ class Results:
 
             if len(cards) > 3:
                 await ctx.send(
-                    f'''Position: **{str.upper(position)}**   \nStack: **{stack}**  \nGTO: **__Open__**''')
+                    f'''Position: **{str.upper(position)}**   \nStack: **{stack}**  \nGTO: **__Open__**\n\n **__Raise First-In__**\n\n **__Call VS__**\n\n **__3-Bet VS__**\n\n **__Squeeze VS__**''')
             else:
-                await ctx.send(f'''Position: **{position}**  \nStack: **{stack}**  \nGTO: **__Open__**''')
+                await ctx.send(
+                    f'''Position: **{str.upper(position)}**  \nStack: **{stack}**  \nGTO: **__Open__**\n\n **__Raise First-In__**\n\n **__Call VS__**\n\n **__3-Bet VS__**\n\n **__Squeeze VS__**''')
         else:
             if len(cards) > 3:
                 await ctx.send(
-                    f'''\nPosition: **{str.upper(position)}**  \nStack: **{stack}**  \nGTO: **__Fold__**''')
+                    f'''\nPosition: **{str.upper(position)}**  \nStack: **{stack}**  \nGTO: **__Fold__**\n\n **__Raise First-In__**\n\n **__Call VS__**\n\n **__3-Bet VS__**\n\n **__Squeeze VS__**''')
             else:
                 await ctx.send(
-                    f'''Position: **{position}**  \nStack: **{stack}**  \nGTO: **__Fold__** ''')
-
-        #     if len(cards) > 3:
-        #         await ctx.send(f'''Position: **{str.upper(position)}**   \nStack: **{stack}**  \nGTO: **__Open__**\n\n **__Raise First-In__**\n\n **__Call VS__**\n\n **__3-Bet VS__**\n\n **__Squeeze VS__**''')
-        #     else: **__Open__**\n\n **__Raise First-In__**\n\n **__Call VS__**\n\n **__3-Bet VS__**\n\n **__Squeeze VS__**''')
-        # else:
-        #     if len(cards) > 3:
-        #         await ctx.send(f'''\nPosition: **{str.upper(position)}**  \nStack: **{stack}**  \nGTO: **__Fold__**\n\n **__Raise First-In__**\n\n **__Call VS__**\n\n **__3-Bet VS__**\n\n **__Squeeze VS__**''')
-        #     else:
-        #         await ctx.send(f'''Position: **{position}**  \nStack: **{stack}**  \nGTO: **__Fold__** \n\n **__Raise First-In__**\n\n **__Call VS__**\n\n **__3-Bet VS__**\n\n **__Squeeze VS__**''')
+                    f'''Position: **{str.upper(position)}**  \nStack: **{stack}**  \nGTO: **__Fold__** \n\n **__Raise First-In__**\n\n **__Call VS__**\n\n **__3-Bet VS__**\n\n **__Squeeze VS__**''')
 
