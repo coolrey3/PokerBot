03760a94319517d19427dd78b071937c1ba0a347
Ray <coolrey3@gmail.com>
2019-12-12 17:04:45 -0500

    make it better

--- Results/__init__.py
+++ Results/__init__.py
@@ -1,19 +1,19 @@
 # Sends text results
+from PositionRanges import PositionRange
 class Results:
-    async def run(ctx,cards,position,stack):
-        if cards in f'''PositionRange.{position}Open''':
 
-            if len(cards) > 3:
-                await ctx.send(
-                    f'''Position: **{str.upper(position)}**   \nStack: **{stack}**  \nGTO: **__Open__**\n\n **__Raise First-In__**\n\n **__Call VS__**\n\n **__3-Bet VS__**\n\n **__Squeeze VS__**''')
-            else:
-                await ctx.send(
-                    f'''Position: **{str.upper(position)}**  \nStack: **{stack}**  \nGTO: **__Open__**\n\n **__Raise First-In__**\n\n **__Call VS__**\n\n **__3-Bet VS__**\n\n **__Squeeze VS__**''')
+    def GTO(cards,position):
+        gtoOutput =f''' PositionRange.{position}Open'''
+        gt = f'{gtoOutput}'
+        print(gt)
+        if cards in f'''PositionRange.{position}Open''':
+            GTOplay = 'Open'
+            return GTOplay
         else:
-            if len(cards) > 3:
-                await ctx.send(
-                    f'''\nPosition: **{str.upper(position)}**  \nStack: **{stack}**  \nGTO: **__Fold__**\n\n **__Raise First-In__**\n\n **__Call VS__**\n\n **__3-Bet VS__**\n\n **__Squeeze VS__**''')
-            else:
-                await ctx.send(
-                    f'''Position: **{str.upper(position)}**  \nStack: **{stack}**  \nGTO: **__Fold__** \n\n **__Raise First-In__**\n\n **__Call VS__**\n\n **__3-Bet VS__**\n\n **__Squeeze VS__**''')
+            GTOplay = 'Fold'
+            return GTOplay
+
+    async def run(ctx,cards,position,stack):
+
+            await ctx.send(f'''Position: **{str.upper(position)}**   \nStack: **{stack}**  \nGTO: **{Results.GTO(cards,position)}**\n\n **__Raise First-In__**\n\n **__Call VS__**\n\n **__3-Bet VS__**\n\n **__Squeeze VS__**''')
 
--- pokerbot.py
+++ pokerbot.py
@@ -43,13 +43,13 @@ async def users(ctx):
 async def poker_bot(ctx):
     embed = discord.Embed(title="PokerBot Help", description='''Hi i'm pokerbot! I am in early alpha so what I am able to do is limited at this time.\n\nRefer to the list below for my available commands and keep in mind commands are case insensitive for your convenience! \n''')
     embed.add_field(name="!PokerBot", value="Displays the Poker Bot instructions")
-    embed.add_field(name="!Range", value="type comman followed by range to see range chart;(i.e: !Range 33+,A5s+")
+    embed.add_field(name="!Range", value="Type command followed by range to see range chart;(i.e: !Range 33+,A5s+")
     embed.add_field(name="!OpenRange", value="Sends image of GTO recommended open range for 6 max NLHE")
     embed.add_field(name="!BTN", value="type command followed by hole cards for an open or fold recommendation from the Button;(i.e: !BTN A9o)")
-    embed.add_field(name="!SB", value="type command followed by hole cards for an open or fold recommendation from the Small Blind;(i.e: !SB JJ)")
-    embed.add_field(name="!CO", value="type command followed by hole cards for an open or fold recommendation from the Cut Off;(i.e: !CO 8h6d)")
-    embed.add_field(name="!MP", value="type command followed by hole cards for an open or fold recommendation from Middle Position;(i.e: !MP T9s)")
-    embed.add_field(name="!UTG", value="type command followed by hole cards for an open or fold recommendation from Under the Gun;(i.e: !UTG AA)")
+    embed.add_field(name="!SB", value="Type command followed by hole cards for an open or fold recommendation from the Small Blind;(i.e: !SB JJ)")
+    embed.add_field(name="!CO", value="Type command followed by hole cards for an open or fold recommendation from the Cut Off;(i.e: !CO 8h6d)")
+    embed.add_field(name="!MP", value="Type command followed by hole cards for an open or fold recommendation from Middle Position;(i.e: !MP T9s)")
+    embed.add_field(name="!UTG", value="Type command followed by hole cards for an open or fold recommendation from Under the Gun;(i.e: !UTG AA)")
     await ctx.send(content=None, embed=embed)
 
 
