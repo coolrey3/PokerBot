09d242f7500c7b406a01a9a2d6df08fc52920e5a
Ray <coolrey3@gmail.com>
2019-12-12 01:55:30 -0500

    Organized image creation and send image in to own methods in CardImages class, importing from separate file

--- pokerbot.py
+++ pokerbot.py
@@ -1,11 +1,11 @@
 import discord
 from poker import *
 from discord.ext import commands
-# from HoleCardImage import CardImages
-import os.path
-from PIL import Image
+from HoleCardImage import CardImages
+# import os.path
+# from PIL import Image
 
-
+cards = ''
 description = '''An example bot to showcase the discord.ext.commands extension
 module. There are a number of utility commands being showcased here.'''
 
@@ -18,19 +18,16 @@ def read_token():
         lines = f.readlines()
         return lines[0].strip()
 
-
 token = read_token()
 client = discord.Client()
 guildId = client.get_guild(635691275663704091)
-
 
 # GTO Opening Ranges
 utgOpen = Range('AA-33 AKo-AJo KQo AKs-ATs KQs-KTs QJs-QTs JTs-J9s T9s 98s 87s 76s 65s')
 mpOpen = Range('AA-22 AKs-A7s A5s KQs-KTs QJs-QTs JTs-J9s T9s-T8s 98s-97s 87s-86s 76s-75s 65s 54s AKo-ATo KQo')
 coOpen = Range('AA-22 AKs-A2s KQs-K6s QJs-Q7s JTs-J8s T9s-T8s 98s-97s 87s-86s 76s-75s 65s-64s 54s AKo-ATo KQo-KJo QJo')
 buttonOpen = Range('AA-22 AKs-A2s KQs-K2s QJs-Q2s JTs-J5s T9s-T6s 98s-96s 87s-85s 76s-74s 65s-64s 54s-53s 43s AKo-A2o KQo-K7o QJo-Q9o JTo-J9o T9o-T8o 98o 87o')
-sbOpen = Range(
-    'AA-22 AKs-A2s KQs-K2s QJs-Q4s JTs-J7s T9s-T7s 98s-97s 87s-86s 76s-75s 65s-64s 54s AKo-A7o KQo-K9o QJo-Q9o JTo-J9o T9o 98o')
+sbOpen = Range('AA-22 AKs-A2s KQs-K2s QJs-Q4s JTs-J7s T9s-T7s 98s-97s 87s-86s 76s-75s 65s-64s 54s AKo-A7o KQo-K9o QJo-Q9o JTo-J9o T9o 98o')
 
 
 @bot.event
@@ -45,10 +42,11 @@ async def users(ctx):
     await ctx.send(f"""# of users on this server: {guildId.member_count}""")
 
 
-@bot.command(aliases=("pokerbot",'pb'))
+@bot.command(aliases=("pokerbot", 'pb'))
 async def poker_bot(ctx):
     embed = discord.Embed(title="PokerBot Help", description='''Hi i'm pokerbot! I am in early alpha so what I am able to do is limited at this time.\n\nRefer to the list below for my available commands and keep in mind commands are case insensitive for your convenience! \n''')
     embed.add_field(name="!PokerBot", value="Displays the Poker Bot instructions")
+    embed.add_field(name="!Range", value="type comman followed by range to see range chart;(i.e: !Range 33+,A5s+")
     embed.add_field(name="!OpenRange", value="Sends image of GTO recommended open range for 6 max NLHE")
     embed.add_field(name="!BTN", value="type command followed by hole cards for an open or fold recommendation from the Button;(i.e: !BTN A9o)")
     embed.add_field(name="!SB", value="type command followed by hole cards for an open or fold recommendation from the Small Blind;(i.e: !SB JJ)")
@@ -68,56 +66,21 @@ async def sb(ctx, cards, stack="100bb"):
     cardsFormatted = str.upper(cards[:2])+cards[2:]
     position = 'SB'
 
-    class CardImages:
-
-         async def cardimage(self):
-
-            print(cards + ' in class')
-            if len(cards) > 3:
-                card1 = cards[:2]
-                card2 = cards[2:4]
-
-                # checks to see if hole cards already exist in folder
-                fp = './HoleCards/'
-                if os.path.isfile(f'''{fp}{cards}.png'''):
-                    print('Hole Cards already exist')
-                else:
-                    images = [Image.open(x) for x in [f'''./4colordeck/{card1}.png''', f'''./4colordeck/{card2}.png''']]
-                    widths, heights = zip(*(i.size for i in images))
-
-                    total_width = sum(widths)
-                    max_height = max(heights)
-
-                    new_im = Image.new('RGB', (total_width, max_height))
-
-                    x_offset = 0
-
-                    for im in images:
-                        new_im.paste(im, (x_offset, 0))
-                        x_offset += im.size[0]
-
-                    new_im.save(f'''./HoleCards/{cards}.png''')
-                    print('new image saved in holecards')
-
-                # sends images of cards
-                file = discord.File(f'''./HoleCards/{cards}.png''', filename=f'''./HoleCards/{cards}.png''')
-                await ctx.send(file=file)
-
-                ############################
-
     await CardImages.cardimage(cards)
+    await ctx.send(file= CardImages.sendimage(cards))
+###################################
 
     # sends text results
     if cards in sbOpen:
         if len(cards) > 3:
-            await ctx.send(f'''Position: **{position}**   \nStack: **{stack}**  \nGTO: **__Open__**''')
+            await ctx.send(f'''Position: **{position}**   \nStack: **{stack}**  \nGTO: **__Open__**\n\n **__Raise First-In__**\n\n **__Call VS__**\n\n **__3-Bet VS__**\n\n **__Squeeze VS__**''')
         else:
-            await ctx.send(f'''Hand: ** {cardsFormatted} **  \nPosition: **{position}**  \nStack: **{stack}**  \nGTO: **__Open__**''')
+            await ctx.send(f'''Hand: ** {cardsFormatted} **  \nPosition: **{position}**  \nStack: **{stack}**  \nGTO: **__Open__**\n\n **__Raise First-In__**\n\n **__Call VS__**\n\n **__3-Bet VS__**\n\n **__Squeeze VS__**''')
     else:
         if len(cards) > 3:
-            await ctx.send(f'''\nPosition: **{position}**  \nStack: **{stack}**  \nGTO: **__Fold__**''')
+            await ctx.send(f'''\nPosition: **{position}**  \nStack: **{stack}**  \nGTO: **__Fold__**\n\n **__Raise First-In__**\n\n **__Call VS__**\n\n **__3-Bet VS__**\n\n **__Squeeze VS__**''')
         else:
-            await ctx.send(f'''Hand: ** {cardsFormatted} **  \nPosition: **{position}**  \nStack: **{stack}**  \nGTO: **__Fold__**''')
+            await ctx.send(f'''Hand: ** {cardsFormatted} **  \nPosition: **{position}**  \nStack: **{stack}**  \nGTO: **__Fold__** \n\n **__Raise First-In__**\n\n **__Call VS__**\n\n **__3-Bet VS__**\n\n **__Squeeze VS__**''')
 
 @bot.command()
 async def utg(ctx, arg, stack="100bb"):
@@ -163,6 +126,13 @@ async def btn(ctx, arg, stack="100bb"):
 async def range(ctx, cards):
     print(cards)
     await ctx.send(Range(cards).to_ascii())
+
+@bot.command()
+async def setrange(ctx, position, userrange):
+    print(position)
+    if str.lower(position) == 'sb':
+        sbOpen = Range(userrange)
+    await ctx.send(Range(userrange).to_ascii())
 
 
 bot.run(token)
